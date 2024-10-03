6ec0dd966769252730e61bb130a3254ca27ac49f
Shivam Chopra <parthasus7973@gmail.com>
2024-10-03 22:38:51 +0530

    Connect with DB

--- src/components/formPost.tsx
+++ src/components/formPost.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { FormInputPost } from "@/types";
+import { Tag } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface FormPostProps {
@@ -9,6 +12,14 @@ interface FormPostProps {
 
 const FormPost = ({ submit, isEditing }: FormPostProps) => {
   const { register, handleSubmit } = useForm<FormInputPost>();
+  const { data: dataTags, isLoading: isLoadingTags } = useQuery<Tag[]>({
+    queryKey: ["tags"],
+    queryFn: async () => {
+      const respone = await axios.get("/api/tags");
+      return respone.data;
+    },
+  });
+  // console.log(dataTags);
 
   return (
     <form
@@ -21,24 +32,31 @@ const FormPost = ({ submit, isEditing }: FormPostProps) => {
         placeholder="Post Title"
         className="input input-bordered w-full max-w-lg text-blue-500"
       />
- 
+
       <textarea
         {...(register("content"), { required: true })}
         className="textarea textarea-bordered w-full max-w-lg text-white"
         placeholder="Post Content"
       ></textarea>
 
-      <select
-        {...(register("tag"), { required: true })}
-        className="select select-bordered w-full max-w-lg text-white"
-        defaultValue=""
-      >
-        <option disabled value="">
-          Select Tags
-        </option>
-        <option>JavaScript</option>
-        <option>C++</option>
-      </select>
+      {isLoadingTags ? (
+        <span className="loading loading-bars loading-md"></span>
+      ) : (
+        <select
+          {...(register("tag"), { required: true })}
+          className="select select-bordered w-full max-w-lg text-white"
+          defaultValue=""
+        >
+          <option disabled value="">
+            Select Tags
+          </option>
+          {dataTags?.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      )}
 
       <button
         type="submit"
